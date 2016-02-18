b8e530291523aa4f1cabcfb63ff7fffeae59d12a
“Milan <myroadtocode@gmail.com>
2016-02-18 12:24:11 +0100

    Dodata lekcija 11

--- object.php
+++ object.php
@@ -7,41 +7,52 @@ $object = array(
 	'code' => '',
 	'date' => ''
 );
-if ( $id == 1 ){
-	$object = array (
 
-		'title' => 'Programming Question #1',
-		'question' => 'I\'m having trouble displaying JavaScript alert',
-		'description' => 'I think I\'m using the correct function, but I can\'t figure out what\'s wrong. Could you point me in the right direction?',
-		'code' => 'alert(This is a message)',
-		'date' => 'February 16, 2016'
-		);
+// Databese connection credentials
+$servername = 'localhost';
+$username = 'homestead';
+$password = 'secret';
+
+// Create connection
+$connection = new mysqli($servername, $username, $password);
+
+// Check for an error
+if ($connection->connect_error) {
+	echo 'Connection failed: '. $connection->connect_error;
+	exit;
 }
-elseif ($id == 2) {
-	$object = array (
-		'title' => 'Programming Question #2',
-		'question' => 'My HTML list isn\'t displaying properly.',
-		'description' => 'I think I\'m using the right list element,',
-		'code' => '&lt;ul&gt;
-		           	item 1
-		           	item 2
-		           	item 3
-		         &lt;/ul&gt;',
-		'date' => 'February 16, 2016'
-	);
+// Otherwise connection is successfull
+// echo 'Connected successfully';
+
+// Connect to the "fitl" database
+$connection->select_db('fitl');
+
+// Query to select an object
+$sql = 'SELECT * FROM questions WHERE id = ' . $id;
+
+// Execute the query
+$result = $connection->query($sql);
+
+// Check for and retrieve the object
+if ($result->num_rows > 0){
+	$object = $result->fetch_assoc();
+	// echo '<pre>';
+	// print_r($object);
+	// echo '</pre>';
+
 }
+
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<title><?php echo $object['title']; ?></title>
     </head>
-    	<h1><?php echo $object['question']; ?></h1>
     	<p><?php echo $object['description']; ?></p>
     	<pre>
     		<?php echo $object['code']; ?>
     	</pre>
-    	<p>Question date: <?php echo $object['date']; ?> </p>
+    	<p>Question date: <?php echo $object['submitted_at']; ?> </p>
     <body>
     </body>
 </html>
